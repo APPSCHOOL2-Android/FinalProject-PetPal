fa77db66a5f7a743a430fae93715e477a4269248
고진호 <90592183+wktkdandp@users.noreply.github.com>
2023-09-05 14:39:52 +0900

    메인화면 ui,유저 바텀 스프레드 시트, 방문지 바텀 스프레드 시트

--- app/src/main/java/com/petpal/mungmate/MainFragment.kt
+++ app/src/main/java/com/petpal/mungmate/MainFragment.kt
@@ -29,6 +29,7 @@ class MainFragment : Fragment() {
         }
 
         return fragmentMainBinding.root
+
     }
 
 }
--- app/src/main/java/com/petpal/mungmate/WalkFragment.kt
+++ app/src/main/java/com/petpal/mungmate/WalkFragment.kt
@@ -5,25 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.petpal.mungmate.databinding.FragmentWalkBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [WalkFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WalkFragment : Fragment() {
+    lateinit var fragmentWalkBinding: FragmentWalkBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_walk, container, false)
+       fragmentWalkBinding= FragmentWalkBinding.inflate(layoutInflater)
+        fragmentWalkBinding.buttonWalk.setOnClickListener {
+            val bottomSheetView = layoutInflater.inflate(R.layout.walk_bottom_sheet_place_layout, null)
+            val bottomSheetDialog = BottomSheetDialog(requireActivity())
+            bottomSheetDialog.setContentView(bottomSheetView)
+            bottomSheetDialog.show()
+//            fragmentWalkBinding.LinearLayoutOffWalk.visibility=View.GONE
+//            fragmentWalkBinding.LinearLayoutOnWalk.visibility=View.VISIBLE
+        }
+
+        fragmentWalkBinding.buttonStopWalk.setOnClickListener {
+            fragmentWalkBinding.LinearLayoutOffWalk.visibility=View.VISIBLE
+            fragmentWalkBinding.LinearLayoutOnWalk.visibility=View.GONE
+
+        }
+        return fragmentWalkBinding.root
     }
 
 }
