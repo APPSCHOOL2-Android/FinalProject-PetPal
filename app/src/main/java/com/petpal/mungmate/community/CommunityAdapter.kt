90ef716124c91c6a69e76368783c59dd25880be1
kimjinsub1217 <hikm9985@naver.com>
2023-09-06 23:33:43 +0900

    design : 커뮤니티 UI 디자인 변경#2

--- app/src/main/java/com/petpal/mungmate/community/CommunityAdapter.kt
+++ app/src/main/java/com/petpal/mungmate/community/CommunityAdapter.kt
@@ -66,8 +66,8 @@ class CommunityAdapter(private val context: Context) :
 
         holder.communityPostTitle.text = "귀여운 강아지 사진"
         holder.communityUserNickName.text = "리트리버군"
-        holder.communityUserPlace.text = "제주특별자치도 제주시 애월읍"
-        holder.communityPostDateCreated.text = "30분전"
+        holder.communityUserPlace.text = "제주시 애월읍"
+        holder.communityPostDateCreated.append("30분전")
         holder.communityContent.text = "귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다.귀여운 리트리버 사진입니다."
         holder.communityFavoriteTextView.text = "좋아요 7"
         holder.communityCommentTextView.text = "댓글 2"
