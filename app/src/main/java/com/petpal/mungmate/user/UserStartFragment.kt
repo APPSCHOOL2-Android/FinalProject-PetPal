1a85ea560043adae62737d6cdccb506b33ce667f
Ji <74065815+Jiheon-Lee98@users.noreply.github.com>
2023-09-07 10:31:09 +0900

    #5 시작하기 화면 완료 todo - kakao, google 더 괜찮은 아이콘 찾아보기 #13 사용자 정보 화면 완료

---
+++ app/src/main/java/com/petpal/mungmate/user/UserStartFragment.kt
@@ -0,0 +1,18 @@
+package com.petpal.mungmate.user
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.petpal.mungmate.R
+
+class UserStartFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_start, container, false)
+    }
+}
