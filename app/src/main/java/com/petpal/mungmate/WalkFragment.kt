4986b6889e26ad2341ca768d8a9e6ba41e9e9cdc
고진호 <90592183+wktkdandp@users.noreply.github.com>
2023-09-07 10:02:02 +0900

    0907

--- app/src/main/java/com/petpal/mungmate/WalkFragment.kt
+++ app/src/main/java/com/petpal/mungmate/WalkFragment.kt
@@ -1,12 +1,15 @@
 package com.petpal.mungmate
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.petpal.mungmate.databinding.FragmentWalkBinding
+
 
 class WalkFragment : Fragment() {
     lateinit var fragmentWalkBinding: FragmentWalkBinding
@@ -30,6 +33,36 @@ class WalkFragment : Fragment() {
             fragmentWalkBinding.LinearLayoutOnWalk.visibility=View.GONE
 
         }
+
+        val spinnerItems = listOf("거리", "1km", "2km")
+        val spinnerItems2 = listOf("성별", "남자", "여자")
+        val spinnerItems3 = listOf("나이대", "10대", "20대","30대")
+        val spinnerItems4 = listOf("견종", "푸들", "시츄","보더콜리")
+
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinnerItems)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        val adapter2 = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinnerItems2)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        val adapter3 = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinnerItems3)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        val adapter4 = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinnerItems4)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        val spinner: Spinner = fragmentWalkBinding.spinner
+        val spinner2: Spinner = fragmentWalkBinding.spinner2
+        val spinner3: Spinner = fragmentWalkBinding.spinner3
+        val spinner4: Spinner = fragmentWalkBinding.spinner4
+
+        spinner.adapter = adapter
+        spinner2.adapter = adapter2
+        spinner3.adapter = adapter3
+        spinner4.adapter = adapter4
+
+
+
         return fragmentWalkBinding.root
     }
 
