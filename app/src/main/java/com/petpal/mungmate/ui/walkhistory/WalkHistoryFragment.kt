ca452ee4cee4eb57770838effea39c0fbde21071
gayeon00 <chlrkdus4819@gmail.com>
2023-09-10 21:11:55 +0900

    feat: #25 mypage ui 수정
    - 마이페이지 다가오는 약속과 매칭기록 붙여주기
    - 마이페이지 차단한 사람 관리 페이지
    - 매칭기록 - 어떤 강아지랑 산책한건지 정보 추가, 필터링 추가

--- app/src/main/java/com/petpal/mungmate/ui/walkhistory/WalkHistoryFragment.kt
+++ app/src/main/java/com/petpal/mungmate/ui/walkhistory/WalkHistoryFragment.kt
@@ -1,4 +1,4 @@
-package com.petpal.mungmate
+package com.petpal.mungmate.ui.walkhistory
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.petpal.mungmate.R
 import com.petpal.mungmate.databinding.FragmentWalkHistoryBinding
 
 class WalkHistoryFragment : Fragment() {
