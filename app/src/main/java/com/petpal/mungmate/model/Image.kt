eafba9be564150155eba4c26bebded9040d61343
kimjinsub1217 <hikm9985@naver.com>
2023-09-13 01:49:14 +0900

    refactor : Image data class @Parcelize 추가해서 Parcelable 구현 #53

--- app/src/main/java/com/petpal/mungmate/model/Image.kt
+++ app/src/main/java/com/petpal/mungmate/model/Image.kt
@@ -2,33 +2,10 @@ package com.petpal.mungmate.model
 
 import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Image(
     var uriString: String? = null,
     var fileName: String? = null
-): Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(uriString)
-        parcel.writeString(fileName)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Image> {
-        override fun createFromParcel(parcel: Parcel): Image {
-            return Image(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Image?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+): Parcelable
