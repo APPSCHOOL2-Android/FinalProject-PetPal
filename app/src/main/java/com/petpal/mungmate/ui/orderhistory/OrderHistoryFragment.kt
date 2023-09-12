5292f68e6a59ea9e4238af71a9b49463a771b5a6
haehyun-lee <bada7144@gmail.com>
2023-09-12 17:58:50 +0900

    [#46] fix : 상품명 최대 2줄 제한, 줄임표

--- app/src/main/java/com/petpal/mungmate/ui/orderhistory/OrderHistoryFragment.kt
+++ app/src/main/java/com/petpal/mungmate/ui/orderhistory/OrderHistoryFragment.kt
@@ -22,17 +22,17 @@ class OrderHistoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _fragmentOrderHistoryBinding = FragmentOrderHistoryBinding.inflate(inflater)
-        
+
         // TODO skeleton 로딩 효과 주기
-        
+
         return fragmentOrderHistoryBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
-        fragmentOrderHistoryBinding.run { 
-            recyclerViewOrderHistory.run { 
+
+        fragmentOrderHistoryBinding.run {
+            recyclerViewOrderHistory.run {
                 adapter = OrderHistoryAdapter(getSampleData())
                 layoutManager = LinearLayoutManager(context)
                 // addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
@@ -42,17 +42,17 @@ class OrderHistoryFragment : Fragment() {
 
     private fun getSampleData() : List<Order> {
         return listOf(
-            Order("2023.09.12", 
+            Order("2023.09.12",
                 listOf(
                     Item("펫팔", "신상 로얄 프리미엄 사료", R.drawable.sample_order_item1, 20000, "퍼피 500g", 2),
                     Item("펫팔", "신상 로얄 프리미엄 캔", R.drawable.sample_order_item2, 15000, "참치맛 150g", 1)
-                ), 
-                "배송중"),
-            Order("2023.09.11",
-                listOf(
-                    Item("펫팔", "신상 로얄 프리미엄 사료", R.drawable.sample_order_item1, 20000, "퍼피 500g", 1),
                 ),
-                "배송완료"),
+                "배송중"),
+            Order("2023.09.11", listOf(Item("펫팔", "신상 로얄 프리미엄 사료", R.drawable.sample_order_item1, 20000, "퍼피 500g", 1),), "배송완료"),
+            Order("2023.09.10", listOf(Item("펫팔", "신상 로얄 프리미엄 사료", R.drawable.sample_order_item1, 20000, "퍼피 500g", 1),), "배송완료"),
+            Order("2023.09.09", listOf(Item("펫팔", "신상 로얄 프리미엄 사료신상 로얄 프리미엄 사료신상 로얄 프리미엄 사료신상 로얄 프리미엄 사료신상 로얄 프리미엄 사료신상 로얄 프리미엄 사료신상 로얄 프리미엄 사료신상 로얄 프리미엄 사료신상 로얄 프리미엄 사료신상 로얄 프리미엄 사료", R.drawable.sample_order_item1, 20000, "퍼피 500g", 1),), "배송완료"),
+            Order("2023.09.09", listOf(Item("펫팔", "신상 로얄 프리미엄 사료", R.drawable.sample_order_item1, 20000, "퍼피 500g", 1),), "배송완료"),
+            Order("2023.09.09", listOf(Item("펫팔", "신상 로얄 프리미엄 사료", R.drawable.sample_order_item1, 20000, "퍼피 500g", 1),), "배송완료")
         )
     }
 
