79a9103ed9ea955b1ea490fbc089f09ed799c9ec
kimjinsub1217 <hikm9985@naver.com>
2023-09-13 11:02:40 +0900

    feat : 커뮤니티 글 작성 자신의 앨범에서 사진 업로드 하기 #47

--- app/src/main/java/com/petpal/mungmate/ui/community/CommunityWritingFragment.kt
+++ app/src/main/java/com/petpal/mungmate/ui/community/CommunityWritingFragment.kt
@@ -1,42 +1,70 @@
 package com.petpal.mungmate.ui.community
 
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.snackbar.Snackbar
+import com.petpal.mungmate.MainActivity
+import com.petpal.mungmate.ProductRegistrationAdapter
 import com.petpal.mungmate.R
 import com.petpal.mungmate.databinding.FragmentCommunityWritingBinding
+import com.petpal.mungmate.model.Image
 
 
 class CommunityWritingFragment : Fragment() {
 
     lateinit var communityWritingBinding: FragmentCommunityWritingBinding
-    private val categoryList = arrayOf(
-        "전체", "일상", "산책일지", "장소후기"
-    )
+    lateinit var mainActivity: MainActivity
+
+    // 이미지 최대 5개
+    private var mainImageList = mutableListOf<Pair<Image, Bitmap>>()
+
+    // 갤러리 실행
+    lateinit var mainGalleryLauncher: ActivityResultLauncher<Intent>
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         communityWritingBinding = FragmentCommunityWritingBinding.inflate(inflater)
         bottomNavigationViewGone()
+        mainGalleryLauncher = mainImageGallerySetting()
+        mainActivity = activity as MainActivity
 
         communityWritingBinding.run {
             toolbar()
 
+            communityWritingPostImageHorizontalRecyclerView.run {
+                adapter = ProductRegistrationAdapter(mainImageList, communityWritingImageButton)
+                layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            }
 
-            val itemAdapter = ArrayAdapter<String>(
-                requireContext(),
-                R.layout.community_category_list, categoryList
-            )
-            categoryItem.setAdapter(itemAdapter)
+            communityWritingImageButton.setOnClickListener {
+                val galleryIntent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI).apply {
+                        type = "image/*"
+                        putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("image/*"))
+                    }
+                mainGalleryLauncher.launch(galleryIntent)
+            }
 
 
         }
@@ -70,6 +98,41 @@ class CommunityWritingFragment : Fragment() {
                 false
             }
         }
+    }
+
+    private fun mainImageGallerySetting(): ActivityResultLauncher<Intent> {
+        val galleryLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                // 메인 이미지 최대 5개까지만 첨부 가능
+                if (mainImageList.count() < 5) {
+                    if (it.resultCode == AppCompatActivity.RESULT_OK && it.data?.data != null) {
+                        val uri = it.data?.data!!
+                        var bitmap: Bitmap? = null
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                            val source =
+                                ImageDecoder.createSource(mainActivity.contentResolver, uri)
+                            bitmap = ImageDecoder.decodeBitmap(source)
+                        } else {
+                            val cursor =
+                                mainActivity.contentResolver.query(uri, null, null, null, null)
+                            if (cursor != null) {
+                                cursor.moveToNext()
+                                val colIdx = cursor.getColumnIndex(MediaStore.Images.Media.DATA)
+                                val source = cursor.getString(colIdx)
+                                bitmap = BitmapFactory.decodeFile(source)
+                            }
+                        }
+                        // 메인 이미지 리스트에 추가
+                        val pairImageInfo = Image(uri.toString(), "") to bitmap!!
+                        mainImageList.add(pairImageInfo)
+                        communityWritingBinding.communityWritingImageButton.text =
+                            "${mainImageList.size}/5"
+                        communityWritingBinding.communityWritingPostImageHorizontalRecyclerView.adapter?.notifyDataSetChanged()
+                    }
+                }
+            }
+        return galleryLauncher
     }
 
 }
